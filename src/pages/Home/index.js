38abfd9b98b04d8b6747454c9acadb60d5764b81
Victor Fontenele <victorvitoria11@hotmail.com>
2020-03-16 19:24:47 -0300

    feat: add to cart

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+import { connect } from 'react-redux';
 
 import api from '../../services/api';
 import { formatPrice } from '../../util/format';
 
 import { ProductList } from './styles';
 
-export default class Home extends Component {
+class Home extends Component {
     state = {
         products: [],
     };
@@ -24,6 +25,15 @@ export default class Home extends Component {
         this.setState({ products: data });
     };
 
+    handleAddProduct = product => {
+        const { dispatch } = this.props;
+
+        dispatch({
+            type: 'ADD_TO_CART',
+            product,
+        });
+    };
+
     render() {
         const { products } = this.state;
 
@@ -36,7 +46,7 @@ export default class Home extends Component {
                             <strong>{ product.title }</strong>
                             <span>{ product.priceFormatted }</span>
 
-                            <button type="button">
+                            <button type="button" onClick={ () => this.handleAddProduct(product) }>
                                 <div>
                                     <MdAddShoppingCart size={16} color="#fff" /> 3
                                 </div>
@@ -49,3 +59,5 @@ export default class Home extends Component {
         );
     };
 };
+
+export default connect()(Home);
