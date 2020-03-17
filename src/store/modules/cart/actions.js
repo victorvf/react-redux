9ed01fa22749aa03745cce18978f0f54e83b19a6
Victor Fontenele <victorvitoria11@hotmail.com>
2020-03-17 10:09:29 -0300

    feat: list of products in cart, cart actions and immer to treat the state

--- src/store/modules/cart/actions.js
+++ src/store/modules/cart/actions.js
@@ -9,5 +9,13 @@ export function removeFromCart(id) {
     return {
         type: '@cart/REMOVE',
         id,
-    }
+    };
+};
+
+export function updateAmount(id, amount) {
+    return {
+        type: '@cart/UPDATE_AMOUNT',
+        id,
+        amount,
+    };
 };
