9783b546be4812d6f2ad79ed7a416766de3235a0
Victor Fontenele <victorvitoria11@hotmail.com>
2020-03-18 16:36:18 -0300

    chore: history/redirect user to cart

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ToastContainer, Zoom } from 'react-toastify';
 
 import './config/ReactotronConfig';
 
 import Routes from './routes';
+import history from './services/history';
 
 import GlobalStyle from './styles/global';
 
@@ -16,14 +17,14 @@ import store from './store';
 function App() {
     return (
         <Provider store={store}>
-            <BrowserRouter>
+            <Router history={history} >
                 <GlobalStyle />
                 <Header />
 
                 <Routes />
 
                 <ToastContainer autoClose={3000} transition={Zoom} />
-            </BrowserRouter>
+            </Router>
         </Provider>
     );
 }
--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,24 +1,18 @@
 import { call, select, put, all, takeLatest } from 'redux-saga/effects';
-<<<<<<< HEAD
 import { toast } from 'react-toastify';
-=======
->>>>>>> 04e5f70626b91420126bc3044d0ad3a8acf467ba
 
 import api from '../../../services/api';
+import history from '../../../services/history';
+
 import { formatPrice } from '../../../util/format';
 
-<<<<<<< HEAD
 import { addToCartSuccess, updateAmountSuccess } from './actions';
-=======
-import { addToCartSuccess, updateAmount } from './actions';
->>>>>>> 04e5f70626b91420126bc3044d0ad3a8acf467ba
 
 function* addToCart({ id }) {
     const productExists = yield select(state =>
         state.cart.find(p => p.id === id)
     );
 
-<<<<<<< HEAD
     const stock = yield call(api.get, `/stock/${id}`);
 
     const stockAmount = stock.data.amount;
@@ -33,12 +27,6 @@ function* addToCart({ id }) {
 
     if (productExists) {
         yield put(updateAmountSuccess(id, amount));
-=======
-    if (productExists) {
-        const amount = productExists.amount + 1;
-
-        yield put(updateAmount(id, amount));
->>>>>>> 04e5f70626b91420126bc3044d0ad3a8acf467ba
     } else {
         const response = yield call(api.get, `/products/${id}`);
 
@@ -49,8 +37,9 @@ function* addToCart({ id }) {
         };
 
         yield put(addToCartSuccess(data));
+
+        history.push('/cart');
     }
-<<<<<<< HEAD
 }
 
 function* updateAmount({ id, amount }) {
@@ -66,8 +55,6 @@ function* updateAmount({ id, amount }) {
     }
 
     yield put(updateAmountSuccess(id, amount));
-=======
->>>>>>> 04e5f70626b91420126bc3044d0ad3a8acf467ba
 }
 
 export default all([
